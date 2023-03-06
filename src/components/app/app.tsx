33fcd5b054680994f85881e2180d444eb03cca52
Amir Farkhadov <farkhadov950405@Amirs-MacBook-Pro.local>
2023-03-06 15:02:04 +0000

    HW2. Implemented MainScreen and Props for it

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,21 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type MainScreenProps = {
+  filmsCount: number;
+  mainFilm: string;
+  mainGenre: string;
+  mainYear: number;
+}
+
+function App({filmsCount, mainFilm, mainGenre, mainYear}: MainScreenProps): JSX.Element {
+  return (
+    <MainScreen
+      filmsCount = {filmsCount}
+      mainFilm = {mainFilm}
+      mainGenre = {mainGenre}
+      mainYear = {mainYear}
+    />
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const MainScreenSettings = {
+  FilmsCount: 20,
+  MainFilm: 'The Grand Budapest Hotel',
+  MainGenre: 'Drama',
+  MainYear: 2014,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmsCount = {MainScreenSettings.FilmsCount}
+      mainFilm = {MainScreenSettings.MainFilm}
+      mainGenre = {MainScreenSettings.MainGenre}
+      mainYear = {MainScreenSettings.MainYear}
+    />
   </React.StrictMode>,
 );
