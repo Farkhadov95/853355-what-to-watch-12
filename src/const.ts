bbcccc03c70d422a4bbc3ecca12180d1702973c4
Amir Farkhadov <farkhadov950405@Amirs-MacBook-Pro.local>
2023-03-09 13:58:18 +0000

    HW3. Added Navigation and PrivateRoute

---
+++ src/const.ts
@@ -0,0 +1,15 @@
+export enum AppRoute {
+    Login = '/login',
+    Player = '/player',
+    Root = '/',
+    Film = '/film',
+    MyList = '/myList',
+    Review = '/review'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN'
+}
+
