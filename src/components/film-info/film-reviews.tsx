938e19041c84aade4ba1d57db0c61fd5e14c0e47
Amir Farkhadov <farkhadov950405@Amirs-MacBook-Pro.local>
2023-04-19 15:11:33 +0100

    HW8. fixed review columns and player video time display

--- src/components/film-info/film-reviews.tsx
+++ src/components/film-info/film-reviews.tsx
@@ -6,12 +6,19 @@ type FilmReviewsProps = {
 }
 
 function FilmReviews({reviews}: FilmReviewsProps): JSX.Element {
-// TODO: create second column for reviews
+  const middleElememt = Math.ceil(reviews.length / 2);
+  const firstColumn = reviews.slice(0, middleElememt);
+  const secondColumn = reviews.slice(middleElememt);
 
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
-        {reviews.map((review) => (
+        {firstColumn.map((review) => (
+          <FilmReviewItem key={review.id} review={review} />
+        ))}
+      </div>
+      <div className="film-card__reviews-col">
+        {secondColumn.map((review) => (
           <FilmReviewItem key={review.id} review={review} />
         ))}
       </div>
--- src/pages/player-screen.tsx/player-screen.tsx
+++ src/pages/player-screen.tsx/player-screen.tsx
@@ -67,6 +67,9 @@ function PlayerScreen(): JSX.Element {
 
 
   const onExitButtonClick = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
     dispatch(redirectToRoute(AppRoute.Root));
   };
 
@@ -90,7 +93,11 @@ function PlayerScreen(): JSX.Element {
     const h = Math.floor(seconds / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
-    return `${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+    if (h === 0) {
+      return `- ${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+    } else {
+      return `- ${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+    }
   }
 
   return (
